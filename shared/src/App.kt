dbde5379a8d375e0d7d12fdb7eed2f7de931f288
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-02-06 07:50:17 +0500

    Initial Commit

---
+++ shared/src/App.kt
@@ -0,0 +1,21 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun App() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text("Hello, ${getWorld()}!")
+        }
+    }
+}
